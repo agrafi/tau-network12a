e1fed89bb4ee4a1cb21952b9638ad858199b52ce
agrafi@gmail.com <agrafi@gmail.com@1a320093-cd6e-cbb1-7b00-fe5daef3c530>
2011-12-01 22:48:06 +0000

    Initial

---
+++ netex1/netex1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : netex1.cpp
+// Author      : Aviv Grafi
+// Version     :
+// Copyright   : 
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("GG");
+	return EXIT_SUCCESS;
+}
